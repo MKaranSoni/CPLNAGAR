a4832121a7ed937c1e80ca1cbc21d9444d4ee216
MKaranSoni <cse.karan2007@gmail.com>
2026-07-15 00:54:13 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 import { getToken } from "./authService";
 
-const api = axios.create({
-  baseURL: "http://localhost:8080",
-});
-
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL, });
 // attach JWT automatically
 api.interceptors.request.use((config) => {
   const token = getToken();
